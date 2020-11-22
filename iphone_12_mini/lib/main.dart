902259c8b0ec21545817f59c1c748809642e7746
Aaron Lee <aaronlab.net@gmail.com>
2020-11-22 19:54:27 +0900

    Update Image Asset

--- iphone_12_mini/lib/main.dart
+++ iphone_12_mini/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-gallery1-2020_FMT_WHH?wid=750&hei=778&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1601402683000'),
+            image: AssetImage('images/iphone_12_mini.png'),
           ),
         ),
       ),
