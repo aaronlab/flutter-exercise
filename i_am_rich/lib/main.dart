720c3e17f02e6a92422241d85186b43600266670
Aaron Lee <aaronlab.net@gmail.com>
2022-01-02 13:36:34 +0900

    NetworkImage

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hell World"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange[100],
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1640622308205-8ad9478c2386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'),
+          ),
+        ),
       ),
     ),
   );
