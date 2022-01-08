47c97f84d0f485b27a8bedafda2cecf10ba6ff13
Aaron Lee <aaronlab.net@gmail.com>
2022-01-08 22:03:15 +0900

    Challenge: Add a scroll listener

--- 05-scrollable-widgets/projects/challenge/challenge-starter/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-starter/lib/screens/explore_screen.dart
@@ -4,10 +4,47 @@ import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
 import '../models/models.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
-  ExploreScreen({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return _ExploreScreen();
+  }
+}
+
+class _ExploreScreen extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _scrollController;
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      _checkListViewTopOrBottom();
+    });
+  }
+
+  /// Check the position of the list view is on the top or bottom.
+  void _checkListViewTopOrBottom() {
+    if (!_scrollController.position.atEdge) { return; }
+
+    final isTop = _scrollController.position.pixels == 0;
+
+    if (isTop) {
+      print('The list view is on the top!');
+      return;
+    }
+
+    print('The list view is on the bottom!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +53,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _scrollController,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
