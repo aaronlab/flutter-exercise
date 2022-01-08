044c083293d571496eb1d0668c6cf2c8597ea804
Aaron Lee <aaronlab.net@gmail.com>
2022-01-08 22:23:27 +0900

    Challenge: Add a new GridViewLayout

--- 05-scrollable-widgets/projects/challenge/challenge-starter/lib/home.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-starter/lib/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    ExploreScreen(),
+    const ExploreScreen(),
     RecipesScreen(),
     Container(color: Colors.blue),
   ];
