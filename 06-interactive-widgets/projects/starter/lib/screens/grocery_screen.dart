bfb9bd127daec2312ec00aa8d5c82e02b1995110
Aaron Lee <aaronlab.net@gmail.com>
2022-03-30 17:12:25 +0900

    Empty Screen

---
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'empty_grocery_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 2: Replace with EmptyGroceryScreen
+    return const EmptyGroceryScreen();
+  }
+
+// TODO: Add buildGroceryScreen
+}
