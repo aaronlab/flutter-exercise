445c8f675d00f026ba671dfad2bdf29ace09b79e
Aaron Lee <aaronlab.net@gmail.com>
2022-04-03 14:56:30 +0900

    provider

---
+++ 12-using-the-chopper-library/projects/starter/lib/data/memory_repository.dart
@@ -0,0 +1,96 @@
+import 'dart:core';
+
+import 'package:flutter/foundation.dart';
+
+import 'models/models.dart';
+import 'repository.dart';
+
+class MemoryRepository extends Repository with ChangeNotifier {
+  final List<Recipe> _currentRecipes = <Recipe>[];
+  final List<Ingredient> _currentIngredients = <Ingredient>[];
+
+  @override
+  void close() {}
+
+  @override
+  void deleteIngredient(Ingredient ingredient) {
+    _currentIngredients.remove(ingredient);
+  }
+
+  @override
+  void deleteIngredients(List<Ingredient> ingredients) {
+    _currentIngredients
+        .removeWhere((ingredient) => ingredients.contains(ingredient));
+    notifyListeners();
+  }
+
+  @override
+  void deleteRecipe(Recipe recipe) {
+    _currentRecipes.remove(recipe);
+
+    if (recipe.id != null) {
+      deleteRecipeIngredients(recipe.id!);
+    }
+
+    notifyListeners();
+  }
+
+  @override
+  void deleteRecipeIngredients(int recipeId) {
+    _currentIngredients
+        .removeWhere((ingredient) => ingredient.recipeId == recipeId);
+    notifyListeners();
+  }
+
+  @override
+  List<Ingredient> findAllIngredients() {
+    return _currentIngredients;
+  }
+
+  @override
+  List<Recipe> findAllRecipes() {
+    return _currentRecipes;
+  }
+
+  @override
+  Recipe findRecipeById(int id) {
+    return _currentRecipes.firstWhere((recipe) => recipe.id == id);
+  }
+
+  @override
+  List<Ingredient> findRecipeIngredients(int recipeId) {
+    final recipe =
+        _currentRecipes.firstWhere((recipe) => recipe.id == recipeId);
+    final recipeIngredients = _currentIngredients
+        .where((ingredient) => ingredient.recipeId == recipe.id)
+        .toList();
+    return recipeIngredients;
+  }
+
+  @override
+  Future init() {
+    return Future.value(null);
+  }
+
+  @override
+  List<int> insertIngredients(List<Ingredient> ingredients) {
+    if (ingredients.length != 0) {
+      _currentIngredients.addAll(ingredients);
+      notifyListeners();
+    }
+    return <int>[];
+  }
+
+  @override
+  int insertRecipe(Recipe recipe) {
+    _currentRecipes.add(recipe);
+
+    if (recipe.ingredients != null) {
+      insertIngredients(recipe.ingredients!);
+    }
+
+    notifyListeners();
+
+    return 0;
+  }
+}
